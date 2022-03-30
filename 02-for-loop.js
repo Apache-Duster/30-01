e28bffa1a761828c182002fbbf95f74ffdacc487
Apache-Duster <mikelopezca@gmail.com>
2022-03-30 15:10:49 +0200

    java commit

---
+++ 02-for-loop.js
@@ -0,0 +1,5 @@
+console.log("hello");
+
+for (let i = 0; i < 25; i++) {
+    console.log(i)
+}
---
+++ 04-nested-loops.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 5; i++ ){
+    for (let j = 0; j < 5; j++) {
+        console.log("i:", i)
+        console.log("j:", j)
+    }
+}
+for (let row = 0; row < 5; column++) {
+    for (let column = 0; column < 5; column++) {
+        console.log("row:", row)
+        console.log("column:", column);
+    }
+}
